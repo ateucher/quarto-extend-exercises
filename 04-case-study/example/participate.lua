1880e2f03dd4ab8d497ae069c01435835a49d907
Charlotte Wickham <cwickham@gmail.com>
2025-09-05 14:31:03 -0700

    Updates after review with Carlos

--- 04-case-study/example/participate.lua
+++ 04-case-study/example/participate.lua
@@ -1,30 +1,29 @@
+local function column(width)
+  return pandoc.Attr("", {'column'}, {width=width})
+end 
+
 Div = function(el)
   if not el.classes:includes("participate") then
     return nil
   end
-  
-  quarto.doc.add_html_dependency({ 
-    name = "participate", version = "1.0.0",
-    stylesheets = {"question.css"},
-  })
 
-  local content = pandoc.Div(el.content)
-  content.classes = {'question'}
+  local content = pandoc.Div(el.content, pandoc.Attr("", {'question'}, {}))
 
-  if not quarto.format.is_html_slide_output() then
+  if not quarto.format.isHtmlSlideOutput() then
     return content
   end
 
   local boilerplate = pandoc.Para(pandoc.Inlines({
-    pandoc.Image("", "Discord-Logo-Black.png", nil, {alt="Discord Logo"}),
+    pandoc.Image("", "Discord-Logo-Black.png", nil, pandoc.Attr("", {}, {alt="Discord Logo"})),
     pandoc.Str("Answer in our "),
     pandoc.Link(pandoc.Str("Discord"), pandoc.utils.stringify(quarto.metadata.get("discord")))
   }))
   
   local inner_columns = pandoc.Blocks({
-    pandoc.Div(content, {class='column', width='75%'}), 
-    pandoc.Div(boilerplate, {class='column', width='25%'})
+    pandoc.Div(content, column('75%')), 
+    pandoc.Div(boilerplate, column('25%'))
   })
-  local result = pandoc.Div(inner_columns, {class = 'columns'})
+  local result = pandoc.Div(inner_columns, pandoc.Attr("", {"columns"}, {}))
   return result
 end
+
