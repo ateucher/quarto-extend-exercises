cbf884ad6f4435f70ed73f35c825985a0e7a284c
Charlotte Wickham <cwickham@gmail.com>
2025-09-11 14:25:37 -0700

    Target divs <-> Simon says

---
+++ 03-filters/your-turns/3-simon-says/says.lua
@@ -0,0 +1,3 @@
+Span = function(el)
+  return nil
+end
--- 03-filters/your-turns/3-target-divs/shout.lua
+++
@@ -1,9 +0,0 @@
-Span = function(el)
-  if el.classes:includes("shout") then
-    return el:walk({
-      Str = function (el) 
-        return pandoc.Str(pandoc.text.upper(el.text))
-      end
-    }).content
-  end  
-end
