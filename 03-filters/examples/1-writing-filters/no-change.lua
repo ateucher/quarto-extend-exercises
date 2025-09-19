0a915c4c563b0fd31cb4f17236da1a7e30592982
Andy Teucher <andy.teucher@gmail.com>
2025-09-19 08:04:05 -0400

    workshop exercises

--- 03-filters/examples/1-writing-filters/no-change.lua
+++ 03-filters/examples/1-writing-filters/no-change.lua
@@ -1,3 +1,7 @@
 Strong = function(el)
-  return nil
+  -- print debugging
+  quarto.log.output("Hello")
+  quarto.log.output(el)
+  -- Transform all Strong nodes to Emph nodes
+  return pandoc.Emph(el.content)
 end
--- 04-case-study/your-turns/1-add-filter/participate.lua
+++ 04-case-study/your-turns/1-add-filter/participate.lua
@@ -1,3 +1,12 @@
 Div = function(el)
-  return nil
+  if not el.classes:includes("participate") then
+    return nil
+  end
+
+  local result = pandoc.Blocks({})
+  result:extend(el.content)
+  -- create a Para element as it is a block element
+  result:insert(pandoc.Para(pandoc.Strong(pandoc.Str("Answer in our Discord!"))))
+
+  return result
 end
